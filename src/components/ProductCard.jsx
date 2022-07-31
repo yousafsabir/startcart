a2b32f56bd45dcee80184916613f1aedece8c9b6
yousafsabir <yousafsabir430516@gmail.com>
2022-07-31 11:48:35 +0500

    pushing to git

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import "./ProductCard.css";
 
 const ProductCard = (props) => {
+    const isUserPresent = useSelector((state) => state.auth.isPresent);
     const navigate = useNavigate();
+
+    const addToCart = () => {
+        if (!isUserPresent) {
+            navigate("/login");
+            toast("⚠ You need to first login", {
+                style: { backgroundColor: "yellow" },
+            });
+        }
+    };
     return (
         <div className="relative my-3 flex h-[375px] w-[250px] flex-col justify-between overflow-visible rounded bg-gray-100 p-3 shadow-card">
             <div>
@@ -43,7 +54,10 @@ const ProductCard = (props) => {
                     <span className="text-xl font-bold">
                         &#8360;{props.price}
                     </span>
-                    <div className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-gray-300 transition-all hover:h-10 hover:w-10">
+                    <div
+                        className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-gray-300 transition-all hover:h-10 hover:w-10"
+                        onClick={() => addToCart()}
+                    >
                         <AiOutlineShoppingCart className="text-xl" />
                     </div>
                 </div>
