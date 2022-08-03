6a134e5f8ce5e0250b3c9d3f9d41fb13ff0b0a00
yousafsabir <yousafsabir430516@gmail.com>
2022-08-03 16:24:36 +0500

    added doesn't work

--- src/components/navbar/NavProfile.jsx
+++ src/components/navbar/NavProfile.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { logout } from "../../redux/slices/Auth";
 import { VscTriangleDown, VscTriangleUp } from "react-icons/vsc";
 import { BsFillPersonFill } from "react-icons/bs";
+import toast from "react-hot-toast";
 
 const NavProfile = () => {
     const dispatch = useDispatch();
@@ -14,6 +15,18 @@ const NavProfile = () => {
     const submit = () => {
         dispatch(logout());
         navigate("/");
+    };
+
+    const doesntWork = () => {
+        toast(
+            "This section doesn't work right now, but we'll make it work very soon 😊",
+            {
+                style: {
+                    color: "#6d0f2c",
+                    backgroundColor: "#FECDD3",
+                },
+            }
+        );
     };
     return (
         <div
@@ -32,14 +45,23 @@ const NavProfile = () => {
                 className="absolute left-0 right-0 top-[105%] cursor-default space-y-2 rounded bg-gray-50 p-2"
                 style={subMenu ? { dispaly: "block" } : { display: "none" }}
             >
-                <li className="cursor-pointer rounded-sm p-2 py-[2px] hover:bg-gray-100">
+                <li
+                    className="cursor-pointer rounded-sm p-2 py-[2px] hover:bg-gray-100"
+                    onClick={doesntWork}
+                >
                     My Profile
                 </li>
-                <li className="cursor-pointer rounded-sm p-2 py-[2px] hover:bg-gray-100">
+                <li
+                    className="cursor-pointer rounded-sm p-2 py-[2px] hover:bg-gray-100"
+                    onClick={doesntWork}
+                >
                     My Orders
                 </li>
                 <li className="h-[2px] w-full bg-gray-400"></li>
-                <li className="cursor-pointer rounded-sm p-2 py-[2px] hover:bg-gray-100">
+                <li
+                    className="cursor-pointer rounded-sm p-2 py-[2px] hover:bg-gray-100"
+                    onClick={doesntWork}
+                >
                     Help
                 </li>
                 <li
